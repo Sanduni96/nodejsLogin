e10d0da40bb123f99444c21276c3b1b85b96aaf0
“Sanduni <sanduni_imalka@yahoo.com>
2024-04-03 02:11:55 +0530

    final

--- login.js
+++ login.js
@@ -1,9 +1,12 @@
 const mysql = require('mysql2');
 const express = require ("express");
-
+const bodyParser = require("body-parser");
 
 const app = express();
+const encoder = bodyParser.urlencoded();
+
 app.use("/assets",express.static("assets"));
+app.use(encoder);
   
 const connection = mysql.createConnection({
     host: 'localhost', 
@@ -18,17 +21,27 @@ connection.connect(function(error) {
     else console.log('Connected to MySQL');
   });
 
-  app.get("/", function(req, res) {
+  app.get("/", function (req, res) {
     res.sendFile(__dirname + '/index.html');
-  })
+});
 
-  app.post("/",function(req,res){
-    connection.query("select * from loginuser where user_name ? and user_password = ?",function(error,results,fields){
-      is (results.length > 0) {
-        req.redirect("/welcome.html")
-      }
-    })
+  app.post("/", function (req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    connection.query("SELECT * FROM loginuser WHERE user_name = ? AND user_password = ?", [username, password], function (error, results, fields) {
+        if (results.length > 0) {
+            res.redirect("/welcome");
+        } else {
+            res.redirect("/");
+        }
+    });
+});
+
+  //when login success
+  app.get("/welcome", function(req,res){
+    res.sendFile(__dirname + "/welcome.html")
   })
   //set app port
-  app.listen(4500);
+  app.listen(4000);
 
